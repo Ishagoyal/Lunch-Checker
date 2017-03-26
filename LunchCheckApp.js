88bfc080a8f1bb30a116e027bbf2945356e9f200
Isha Goyal <iamishagoyal@gmail.com>
2017-03-26 23:35:47 +0530

    initial angularjs file of the app

---
+++ LunchCheckApp.js
@@ -0,0 +1,11 @@
+(function () {
+	// body...
+	'user strict';
+	angular.module('LunchCheck'[])
+	.controller('LunchCheckController', DIController);
+
+	DIController.$inject=['$scope'];
+	function DIController($scope) {
+		// body...
+	}
+})();
