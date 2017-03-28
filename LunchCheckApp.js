ce515fea3a9cdad8e4b0c16d6b4f7795270309fe
Isha Goyal <iamishagoyal@gmail.com>
2017-03-28 16:50:08 +0530

    app working

--- LunchCheckApp.js
+++ LunchCheckApp.js
@@ -1,11 +1,36 @@
 (function () {
 	// body...
-	'user strict';
-	angular.module('LunchCheck'[])
-	.controller('LunchCheckController', DIController);
+	'use strict';
+	angular.module('LunchCheck', [])
+	.controller('LunchCheckController', LunchCheckController);
 
-	DIController.$inject=['$scope'];
-	function DIController($scope) {
-		// body...
+	LunchCheckController.$inject=['$scope'];
+	function LunchCheckController($scope) {
+		$scope.userInput="";
+
+		$scope.checkLunch=function(){
+			var checkLength = calculateLength($scope.userInput);
+			if(checkLength<=3 && checkLength>0){
+				$scope.message="Enjoy!"
+			}
+
+			else if(checkLength == 0){
+				$scope.message="Please enter data first";
+			}
+
+			else{
+				$scope.message="Too much!";
+			}
+		};
+
+
+		function calculateLength(string) {
+			// body...
+			var arrayLength=0;
+			var userArray = string.split(",");
+			arrayLength = userArray.length;
+
+			return arrayLength;
+		}
 	}
 })();
